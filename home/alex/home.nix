445b004e288e6fa6d0670d9c9f6720707c14c1f3
Alexamakans <fougner.alexander@gmail.com>
2025-10-05 09:49:10 +0200

    git integration user alex

--- home/alex/home.nix
+++ home/alex/home.nix
@@ -87,4 +87,27 @@ in
 
   (mkHomeFileRecursive "/.config/kitty")
   (mkHomeFileRecursive "/.config/hypr")
+
+  {
+    programs.ssh = {
+      enable = true;
+      extraConfig = ''
+        Host *
+            IdentityAgent ~/.1password/agent.sock
+      '';
+    };
+
+    programs.git = {
+      enable = true;
+      extraConfig = {
+        gpg.format = "ssh";
+        "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+        commit.gpgsign = true;
+
+        user = {
+          signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP6v8sgTuwobr8g+NnGZm72/E9xjgjXjy5IS3QWj3lga";
+        };
+      };
+    };
+  }
 ]
